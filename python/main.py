f8b8fb03538e70caf7e219659580a9c2a8b84071
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2021-02-07 18:11:06 +0100

    ruby2python: convert main

--- python/main.py
+++ python/main.py
@@ -1,23 +1,22 @@
-require_relative "elf_parser"
-require_relative "memory"
-require_relative "cpu"
-require_relative "linux"
+from elf_parser import Elf
+from memory import Memory
+from cpu import Cpu
+from linux import Linux
 
-elf = Elf.new("../progs_to_test/bin/hello_cpp")
 
-puts "is 64 bit: %s" % elf.elf_header.is_64_bit
+elf = Elf("../progs_to_test/bin/hello_cpp")
 
-mem = Memory.new
+mem = Memory()
 
 # Load program segments into the memory
-elf.program_header.entries.each do |ph|
-	puts "writing a segment: %d bytes at position 0x%x" % [ph.data_to_load_to_memory.length, ph.p_vaddr]
-	mem.write(ph.p_vaddr, ph.data_to_load_to_memory)
-end
+for ph in elf.program_header.entries:
+    data = ph.data_to_load_to_memory
+    print("writing a segment: %d bytes at pos 0x%x" % [len(data), ph.p_vaddr])
+    mem.write(ph.p_vaddr, data)
 
-cpu = Cpu.new(mem, elf.elf_header.e_entry, 0x123456789ABCDEF)
+cpu = Cpu(mem, elf.elf_header.e_entry, 0x123456789ABCDEF)
 
-linux = Linux.new(cpu, mem)
+linux = Linux(cpu, mem)
 
 cpu.linux = linux
 
@@ -26,19 +25,18 @@ cpu.linux = linux
 cpu.gs = 0xeeeeeeeeeeee
 cpu.fs = 0xf0f0f0f0f0f0
 
-begin
-	while !cpu.stopped
-		puts "====="
-		puts "pos = 0x%x" % cpu.mem_stream.pos
-		cpu.exectute_next_instruction
-	end
-rescue
-	for i in 0..15
-		print "register #%d -> " % i
-		cpu.register[i].debug
-	end
-	puts "flags: " + cpu.flags.to_s
-	puts "stack: 0x%x" % cpu.stack.pos
+try:
+    while not cpu.stopped:
+        print("=====")
+        print("pos = 0x%x" % cpu.mem_stream.pos)
+        cpu.exectute_next_instruction()
+except Exception as e:
+    for i in range(16):
+        print("register #%d -> " % i)
+        cpu.register[i].debug()
 
-	raise
-end
+    print("flags: " + cpu.flags)
+    print("stack: 0x%x" % cpu.stack.pos)
+
+    print(e.__doc__)
+    print(e.message)
